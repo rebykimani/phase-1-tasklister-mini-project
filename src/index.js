2c56bdb90c0ff92b40b9bd7cc00892573c5a6fe6
beckireby15@gmail.com <beckireby15@gmail.com>
2022-08-28 15:18:04 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const ul = document.querySelector("#tasks");
+    let task = form.querySelector("#new-task-description");
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    if (task.value) {
+      li.textContent = task.value;
+      button.innerText = "X";
+      li.appendChild(button);
+      console.log(li);
+      ul.appendChild(li);
+      task.value = "";
+    }
+  });
 });
